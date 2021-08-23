905038212ed9e8dda3f0668acc8472f8044e8ab0
IR_Kovshar <ikovshar91@gmail.com>
2021-08-23 16:45:15 +0600

    commit#3

--- src/main/java/servlets/AllRequestsServlet.java
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -7,10 +7,16 @@ import templater.PageGenerator;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 public class AllRequestsServlet extends HttpServlet {
 
@@ -29,7 +35,34 @@ public class AllRequestsServlet extends HttpServlet {
                        HttpServletResponse response) throws IOException {
         Map<String, Object> pageVariables = createPageVariablesMap(request);
 
+
+
+        URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged=python&site=stackoverflow");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+
+        GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
+
+        String inputLine;
+
+        StringBuffer kek = new StringBuffer();
+
+        while ((inputLine = bufferedReader.readLine()) != null) {
+            kek.append((inputLine));
+        }
+        bufferedReader.close();
+
         String tag  = request.getParameter("tag");
+
+        Gson gson = new Gson();
+        Item azaz = gson.fromJson(String.valueOf(kek), Item.class);
+
+        pageVariables.put("result", azaz);
 
         response.setContentType("text/html;charset=utf-8");
 
@@ -46,7 +79,7 @@ public class AllRequestsServlet extends HttpServlet {
 
     private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
         Map<String, Object> pageVariables = new HashMap<>();
-        pageVariables.put("result",request.getSession());
+        //pageVariables.put("result",request.getSession());
         return pageVariables;
     }
 }
