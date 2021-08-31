fd1c9e470fd3f49b0cda69fcacbbd62ef8c202d4
Игорь <ikovshar91@gmail.com>
2021-08-31 22:02:51 +0600

    commit#6

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,17 +5,23 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(new AllRequestsServlet()), "/*");
 
-        Server server = new Server(8080);
+        Server server = new Server(9999);
         server.setHandler(context);
 
-        server.start();
-        server.join();
+        try {
+            server.start();
+            server.join();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/servlets/AllRequestsServlet.java
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -3,7 +3,7 @@ package servlets;
 import com.google.gson.Gson;
 import json.Item;
 import json.Root;
-import templater.PageGenerator;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +13,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
@@ -21,68 +20,47 @@ public class AllRequestsServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
-
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
-        pageVariables.put("tag", "");
-        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
-
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("Hello world");
     }
 
-    public void doPost(HttpServletRequest request,
-                       HttpServletResponse response) throws IOException {
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
-
-        String tag  = request.getParameter("tag");
-
-        String name = tag.equals("") ? "1": tag;
-        URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged="+
-              name  +"&site=stackoverflow");
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Content-Type", "application/json");
-
-
-        GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
-
-        String inputLine;
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append((inputLine));
-        }
-        bufferedReader.close();
-
-
-        Gson gson = new Gson();
-        Root root = gson.fromJson(stringBuilder.toString(), Root.class);
-
-        for (Item root1 : root.items){
-            pageVariables.put("result", gson.toJson(root1));
-        }
-        response.setContentType("text/html;charset=utf-8");
-
-        if (tag.equals("") || tag.isEmpty()) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        }
-        else {
-            response.setStatus(HttpServletResponse.SC_OK);
-        }
-        pageVariables.put("tag", tag.equals("") ? "" : tag);
-
-
-        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
-    }
-
-    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
-        Map<String, Object> pageVariables = new HashMap<>();
-        //pageVariables.put("result",request.getSession());
-        return pageVariables;
-    }
+//    public void doPost(HttpServletRequest request,
+//                       HttpServletResponse response) throws IOException {
+//
+//
+//        String tag  = request.getParameter("tag");
+//
+//        String name = tag.equals("") ? "1": tag;
+//        URL url = new URL("https://api.stackexchange.com/2.2/search?pagesize=100&order=desc&sort=creation&tagged="+
+//              name  +"&site=stackoverflow");
+//
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//
+//        connection.setRequestMethod("GET");
+//        connection.setRequestProperty("Content-Type", "application/json");
+//
+//
+//        GZIPInputStream gis = new GZIPInputStream(connection.getInputStream());
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
+//
+//        String inputLine;
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//
+//        while ((inputLine = bufferedReader.readLine()) != null) {
+//            stringBuilder.append((inputLine));
+//        }
+//        bufferedReader.close();
+//
+//
+//        Gson gson = new Gson();
+//        Root root = gson.fromJson(stringBuilder.toString(), Root.class);
+//
+////        for (Item root1 : root.items){
+////         //   pageVariables.put("result", gson.toJson(root1));
+////        }
+//        response.setContentType("text/html;charset=utf-8");
+//
+//        response.getWriter().println("Hello world");
+//    }
 }
